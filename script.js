d536d4b96d56853b43dcf4907535ad324516b06f
Phoenix <drake.mcgarrah@gmail.com>
2019-12-20 13:39:55 -0600

    selected input field values by class name

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-Drake McGarrah 1: 25 PM
 $("#searchButton").on("click", function (e) {
-    var year1 = startYearInput
-    var year2 = endYearInput
+    e.preventDefault();
+    var year1 = $(".startYearInput").val();
+    var year2 = $(".endYearInput").val();
     // &fq=pub_year
     // &fq=begin_date(year)
     // &fq=end_date(year)
@@ -9,7 +9,7 @@ $("#searchButton").on("click", function (e) {
     // end_date(year)
     // begin_date(year1)
     // end_date(year2)
-    var searchTermInput = searchInput.val();
+    var searchTermInput = $(".searchInput").val();
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTermInput + "&api-key=sqgGMtJDGcfNtOGdh6kWm4FqAx5RM2yV";
     $.ajax({
         url: queryURL,
