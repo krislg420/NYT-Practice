73d46b63770e14fecb0315c3bd35d216a2bbd478
Phoenix <drake.mcgarrah@gmail.com>
2019-12-20 13:48:24 -0600

    added scripts to bottom of body, now we are able to receive articles as a response from the NYT api

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ $("#searchButton").on("click", function (e) {
     // end_date(year)
     // begin_date(year1)
     // end_date(year2)
-    var searchTermInput = $(".searchInput").val();
+    var searchTermInput = $(".searchInput").val().trim();
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTermInput + "&api-key=sqgGMtJDGcfNtOGdh6kWm4FqAx5RM2yV";
     $.ajax({
         url: queryURL,
